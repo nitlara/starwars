a0f143bc5cded2c1058a29e2a0ccab2844e02f90
nitlara <nitlara@gmail.com>
2021-01-28 09:23:58 +0000

    cards have always the same width (responsive)

--- src/front/js/views/crew.js
+++ src/front/js/views/crew.js
@@ -3,12 +3,12 @@ import { Context } from "../store/appContext";
 import { Character } from "../component/character";
 import "../../styles/home.scss";
 import Card from "react-bootstrap/Card";
-
+import CardDeck from "react-bootstrap/CardDeck";
 export const Crew = () => {
 	const { store, actions } = useContext(Context);
 
 	const listItems = store.characters.map((c, index) => (
-		<Card className="card col-3 text-left align-items-center" key={index}>
+		<Card className="individualcard text-left align-items-center flex-shrink-0 mr-3" key={index}>
 			<Character uid={c.uid} name={c.name} />
 		</Card>
 	));
@@ -16,7 +16,9 @@ export const Crew = () => {
 	return (
 		<div className="text-center mt-5">
 			<div className="container">
-				<div className="scrolling-wrapper row flex-row flex-nowrap ">{listItems}</div>
+				<div className="scrolling-wrapper d-flex justify-content-between row flex-row flex-nowrap">
+					{listItems}
+				</div>
 			</div>
 		</div>
 	);
